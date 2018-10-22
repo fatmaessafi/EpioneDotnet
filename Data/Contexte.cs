b898d886ecfb85f50edd985d818588a88145dacc
Fatma Essafi <fatma.essafi@esprit.tn>
2018-10-22 20:11:52 +0200

    Initial commit

---
+++ Data/Contexte.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Contexte: DbContext
+    {
+        public Contexte(): base("name=DB")
+        {
+
+        }
+    }
+}
