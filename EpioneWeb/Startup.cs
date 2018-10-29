4bf6e4b433959ac351e9a591f5cbde37dcbf1dd9
Fatma Essafi <fatma.essafi@esprit.tn>
2018-10-29 20:54:10 +0100

    Layout and half identity

---
+++ EpioneWeb/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartupAttribute(typeof(EpioneWeb.Startup))]
+namespace EpioneWeb
+{
+    public partial class Startup
+    {/*
+       public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookies",
+                LoginPath = new PathString("/auth/login")
+            });
+                
+          
+        }*/
+    }
+}
